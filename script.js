fcbad8fc2cc1886056760c06907b05cf6817ae9b
lam <i.mailed.lam@gmail.com>
2023-11-13 14:46:30 +0200

    Data to text name

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("what's your name ?");
+var birthYear = prompt("what year were you born ? (ex.1988)");
+
+console.log(`your name is ${name} and you're ${2023 - birthYear} years old.`);
